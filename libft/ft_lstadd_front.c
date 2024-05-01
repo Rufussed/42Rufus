a35e06c2c19842b1584007af956fc091a7b082a9
Rufussed <rufusjlane@gmail.com>
2024-05-01 18:21:10 +0200

    finished required, started bonus2

--- libft/ft_lstadd_front.c
+++ libft/ft_lstadd_front.c
@@ -1,18 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_lstaddfront.c                                   :+:      :+:    :+:   */
+/*   ft_lstadd_front.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: rlane <rlane@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/01 13:48:25 by rlane             #+#    #+#             */
-/*   Updated: 2024/05/01 14:43:34 by rlane            ###   ########.fr       */
+/*   Updated: 2024/05/01 17:55:20 by rlane            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-#include "ft_lstnew.c"
+// #include "ft_lstnew.c"
 
 // Allocates (with malloc(3)) and returns a new node.
 // The member variable ’content’ is initialized with
@@ -21,44 +21,30 @@
 // 1. set new element next adress to point to the start of the existing list
 // set the existing list pointer to point to the new element
 
-void ft_lstadd_front(t_list **lst, t_list *new)
+void	ft_lstadd_front(t_list **lst, t_list *new)
 {
-    new -> next = *lst;
-    *lst = new;
+	new -> next = *lst;
+	*lst = new;
 }
+/*
 #include <string.h>  // Include for strcmp
 int	main(void)
 {
-	t_list	*list = NULL; // Initialize to NULL for the empty list scenario.
+	t_list	*list;
 	t_list	*new;
 	char	*str1 = "Hello";
 	char	*str2 = "World";
 
-	// Creating first list node
 	list = ft_lstnew(str1);
-	if (list == NULL)
-		return (1); // Handling potential allocation failures
-
-	// Creating new node to be added at the front
 	new = ft_lstnew(str2);
-	if (new == NULL)
-	{
-		free(list); // Clean up previously allocated memory
-		return (1);
-	}
-
-	// Initial list content
+	
 	printf("\nInitial content: %s\n", (char *) list->content);
 
-	// Adding new node to the front of the list
 	ft_lstadd_front(&list, new);
 
-	// Content after adding new at the front
 	printf("\nNew front content: %s\n", (char *) list->content);
 
-	// Freeing nodes
-	free(list->next); // Free the original first node
-	free(list);       // Free the new front node
-
+	free(list->next);
+	free(list);
 	return (0);
-}
+}*/
--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: rlane <rlane@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/23 10:19:08 by rlane             #+#    #+#             */
-/*   Updated: 2024/05/01 14:08:45 by rlane            ###   ########.fr       */
+/*   Updated: 2024/05/01 18:16:21 by rlane            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -59,5 +59,13 @@ typedef struct s_list
 }	t_list;
 
 t_list	*ft_lstnew(void *content);
+void	ft_lstadd_front(t_list **lst, t_list *new);
+int		ft_lstsize(t_list *lst);
+t_list	*ft_lstlast(t_list *lst);
+void	ft_lstadd_back(t_list **lst, t_list *new);
+void	ft_lstdelone(t_list *lst, void (*del)(void*));
+void	ft_lstclear(t_list **lst, void (*del)(void*));
+void	ft_lstiter(t_list *lst, void (*f)(void *));
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
 
 #endif
