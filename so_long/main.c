1164bedc5cf05c7d0b0eccaabab6c540f01129f7
Rufussed <rufusjlane@gmail.com>
2024-06-08 10:21:38 +0200

    so-long

--- so_long/main.c
+++ so_long/main.c
@@ -1,67 +1,338 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rlane <rlane@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/06 19:28:56 by rlane             #+#    #+#             */
+/*   Updated: 2024/06/08 10:09:20 by rlane            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "so_long.h"
+#include "libft/src/libft.h"
+#include "mlx/mlx.h"
+#include <X11/keysym.h>
+#include <X11/keysymdef.h>
+#include <X11/X.h>
 
-void draw_line(mlx_image_t *img, int x0, int y0, int x1, int y1, uint32_t color)
+// #define WINDOW_WIDTH 800
+// #define WINDOW_HEIGHT 600
+
+#define BUFF_SIZE 10000
+
+#define MLX_ERROR 1
+
+#define SPRITE_SIZE 48
+
+#define UP 1
+#define RIGHT 2
+#define DOWN 3
+#define LEFT 4
+
+typedef struct s_data
 {
-	int dx = abs(x1 - x0);
-	int dy = abs(y1 - y0);
-	int sx = (x0 < x1) ? 1 : -1;
-	int sy = (y0 < y1) ? 1 : -1;
-	int err = dx - dy;
+	void	*mlx_ptr;
+	void	*win_ptr;
+	void	*floor;
+	void	*wall;
+	void	*exit;
+	void	*elf_up;
+	void	*elf_down;
+	void	*elf_left;
+	void	*elf_right;
+	void	*player;
+	void	*key;
+	char	**map;
+	int		map_width;
+	int		map_height;
+	int		player_x;
+	int		player_y;
+	int		move_x;
+	int		move_y;
+	int		key_count;
+}	t_data;
 
-	while (1)
+
+void	move_player(t_data *data);
+void	draw_map(t_data *data);
+void	draw_game_objects(t_data *data);
+void	check_win(t_data *data);
+
+int	handle_keypress(int keysym, t_data *data)
+{
+	if (keysym == XK_Up)
+	{	
+		data->player = data->elf_up;
+		data->move_y = data->player_y - 1;
+	}
+	if (keysym == XK_Down)
 	{
-		mlx_put_pixel(img, x0, y0, color); // Draw pixel
+		data->player = data->elf_down;
+		data->move_y = data->player_y + 1;
+	}
+	if (keysym == XK_Left)
+	{
+		data->player = data->elf_left;
+		data->move_x = data->player_x - 1;
+	}
+	if (keysym == XK_Right)
+	{
+		data->player = data->elf_right;
+		data->move_x = data->player_x + 1;
+	}
+	move_player(data);
+	draw_map(data);
+	draw_game_objects(data);
+	check_win(data);
+	return (0);
+}
 
-		if (x0 == x1 && y0 == y1)
+int	handle_keyrelease(int keysym, t_data *data)
+{
+	if (keysym == XK_Escape)
+		mlx_destroy_window(data->mlx_ptr, data->win_ptr);
+	return (0);
+}
+
+int	handle_no_event(void *data)
+{
+	/* This function needs to exist, but it is useless for the moment */
+	return (0);
+}
+
+void	load_tiles(t_data *data)
+{
+	int	img_width;
+	int	img_height;
+
+	data->floor = mlx_xpm_file_to_image(data->mlx_ptr, 
+		"assets/floor.xpm", &img_width, &img_height);
+	data->wall = mlx_xpm_file_to_image(data->mlx_ptr,
+		"assets/wall.xpm", &img_width, &img_height);
+	data->exit = mlx_xpm_file_to_image(data->mlx_ptr,
+		"assets/exit.xpm", &img_width, &img_height);
+	data->key = mlx_xpm_file_to_image(data->mlx_ptr,
+		"assets/key.xpm", &img_width, &img_height);
+	data->elf_up = mlx_xpm_file_to_image(data->mlx_ptr,
+		"assets/elf_up.xpm", &img_width, &img_height);
+	data->elf_down = mlx_xpm_file_to_image(data->mlx_ptr,
+		"assets/elf_down.xpm", &img_width, &img_height);
+	data->player = data->elf_down;
+	data->elf_left = mlx_xpm_file_to_image(data->mlx_ptr,
+		"assets/elf_left.xpm", &img_width, &img_height);
+	data->elf_right = mlx_xpm_file_to_image(data->mlx_ptr,
+		"assets/elf_right.xpm", &img_width, &img_height);
+	
+	if (data->floor == NULL || data->elf_left == NULL || data->exit == NULL
+		|| data->elf_up == NULL || data->elf_down == NULL
+		|| data->elf_right == NULL || data->key == NULL)
+		exit(MLX_ERROR);
+}
+
+void	draw_map(t_data *data)
+{
+	int 	i;
+	int 	j;
+
+	i = 0;
+	while (i < data->map_height)
+	{
+		j = 0;
+		while (j < data->map_width)
+		{
+			if (data->map[i][j] == '1')
+				mlx_put_image_to_window(data->mlx_ptr, data->win_ptr, data->wall, j * SPRITE_SIZE, i * SPRITE_SIZE);
+			if (data->map[i][j] == '0' || data->map[i][j] == 'P' || data->map[i][j] == 'C')
+				mlx_put_image_to_window(data->mlx_ptr, data->win_ptr, data->floor, j * SPRITE_SIZE, i * SPRITE_SIZE);
+			if (data->map[i][j] == 'E')
+				mlx_put_image_to_window(data->mlx_ptr, data->win_ptr, data->exit, j * SPRITE_SIZE, i * SPRITE_SIZE);
+				j++;
+		}
+		i++;
+	}	
+}
+
+void draw_game_objects(t_data *data)
+{
+    int i;
+    int j;
+
+    data->key_count = 0;
+    for (i = 0; i < data->map_height; i++)
+    {
+        for (j = 0; j < data->map_width; j++)
+        {
+            if (data->map[i][j] == 'C')
+            {
+                data->key_count++;
+                mlx_put_image_to_window(data->mlx_ptr, data->win_ptr, data->key, j * SPRITE_SIZE, i * SPRITE_SIZE);
+            }
+            if (data->player_x == j && data->player_y == i)
+                mlx_put_image_to_window(data->mlx_ptr, data->win_ptr, data->player, 
+		j * SPRITE_SIZE, i * SPRITE_SIZE);
+        }
+    }
+    if (data->map[data->player_y][data->player_x] == 'C')
+    {
+        data->map[data->player_y][data->player_x] = '0'; 
+        data->key_count--; 
+    }
+}
+
+void	check_win(t_data *data)
+{
+	if (data->key_count == 0 && data->map[data->player_y][data->player_x] == 'E')
+	{
+		mlx_destroy_window(data->mlx_ptr, data->win_ptr);
+		ft_printf("You win!\n");
+	}
+}
+
+int	check_move(t_data *data)
+{
+	if (data->map[data->move_y][data->move_x] == '1' 
+		|| (data->map[data->move_y][data->move_x] == 'E' && data->key_count > 0))
+
+	{
+		data->move_x = data->player_x;
+		data->move_y = data->player_y;
+		return (0);
+	}
+	return (1);		
+}
+
+
+void	move_player(t_data *data)
+{
+	if (check_move(data))
+	{
+		data->player_x = data->move_x;
+		data->player_y = data->move_y;
+	}
+}
+
+
+int	open_window(t_data *data) // Changed data to pointer
+{
+	void	*img;
+	int	img_width;
+	int	img_height;
+	int	win_w;
+	int	win_h;
+
+	data->mlx_ptr = mlx_init();
+	if (data->mlx_ptr == NULL)
+		return (MLX_ERROR);
+	win_w = data->map_width * SPRITE_SIZE;
+	win_h = data->map_height * SPRITE_SIZE;
+	ft_printf("Player x = %d\n", data->player_x);
+	ft_printf("Player y = %d\n", data->player_y);
+	ft_printf("Map width = %d\n", data->map_width);
+	ft_printf("Map height = %d\n\n", data->map_height);
+	ft_printf("Window width = %d\n", win_w);
+	ft_printf("Window height = %d\n\n", win_h);
+	data->win_ptr = mlx_new_window(data->mlx_ptr, win_w, win_h, "So_Long");
+
+	if (data->win_ptr == NULL)
+	{
+		free(data->win_ptr);
+		return (MLX_ERROR);
+	}
+
+	/* Setup hooks */
+	mlx_loop_hook(data->mlx_ptr, &handle_no_event, data);
+	mlx_hook(data->win_ptr, KeyPress, KeyPressMask, &handle_keypress, data);
+	mlx_hook(data->win_ptr, KeyRelease, KeyReleaseMask, &handle_keyrelease, data);
+	load_tiles(data);
+	draw_map(data);
+	//draw_game_objects(data);
+	
+
+
+
+	mlx_loop(data->mlx_ptr);
+
+	/* we will exit the loop if there's no window left, and execute this code */
+	mlx_destroy_display(data->mlx_ptr);
+	free(data->mlx_ptr);
+}
+
+char	**read_map(t_data *data) // Changed data to pointer
+{
+	char	*read_buf;
+	ssize_t	bytes_read;
+	int		fd;
+	size_t	i;
+	size_t	j;
+
+	read_buf = malloc(BUFF_SIZE * sizeof(char));
+	if (!read_buf)
+		return (NULL);
+	fd = open("map.ber", O_RDONLY);
+	bytes_read = read(fd, read_buf, BUFF_SIZE);
+	if (bytes_read == -1)
+		return (NULL);
+	data->map = ft_split(read_buf, '\n');
+	data->map_width = ft_strlen(data->map[0]);
+	i = 0;
+	while (data->map[i])
+	{
+		if (ft_strlen(data->map[i]) != data->map_width)
+			return (NULL);
+		ft_printf("%s\n", data->map[i]);
+		i++;
+	}
+	data->map_height = i;
+	i = 0;
+	while (data->map[i])
+	{
+		j = 0;
+		while (data->map[i][j] && data->map[i][j] != 'P')
+			j++;
+		if (data->map[i][j] == 'P')
+		{
+			data->player_x = j;
+			data->player_y = i;
 			break;
-		int e2 = 2 * err;
-		if (e2 > -dy)
-		{
-			err -= dy;
-			x0 += sx;
 		}
-		if (e2 < dx)
-		{
-			err += dx;
-			y0 += sy;
-		}
+		i++;
 	}
+	ft_printf("Player x = %d\n", data->player_x);
+	ft_printf("Player y = %d\n", data->player_y);
+	ft_printf("Map width = %d\n", data->map_width);
+	ft_printf("Map height = %d\n\n", data->map_height);
+	return (data->map);
 }
 
-int32_t main(void)
+int	main(void)
 {
-	mlx_t	*mlx;
-	mlx_image_t *img;
+	t_data	data;
 
-	// Initialize the MLX42 library and create a window
-	mlx = mlx_init(800, 600, "Draw Line", false);
-	if (!mlx)
+	
+	if (!read_map(&data)) // Pass data by reference
 	{
-		fprintf(stderr, "Failed to initialize MLX42\n");
-		return EXIT_FAILURE;
+		ft_printf("Error reading map\n");
+		return (1);
 	}
 
-	// Create an image
-	img = mlx_new_image(mlx, 800, 600);
-	if (!img)
-	{
-		fprintf(stderr, "Failed to create image\n");
-		mlx_terminate(mlx);
-		return EXIT_FAILURE;
-	}
+	ft_printf("Player x = %d\n", data.player_x);
+	ft_printf("Player y = %d\n", data.player_y);
+	ft_printf("Map width = %d\n", data.map_width);
+	ft_printf("Map height = %d\n\n", data.map_height);
 
-	// Draw a line from (100, 100) to (700, 500)
-	draw_line(img, 100, 100, 700, 500, 0xFFFFFFFF);
+	data.move_x = data.player_x;
+	data.move_y = data.player_y;
 
-	// Display the image in the window
-	mlx_image_to_window(mlx, img, 0, 0);
+	// Now open the window and draw the map
+	open_window(&data);
 
-	// Enter the MLX42 event loop
-	mlx_loop(mlx);
+	// Free the map after usage
+	for (size_t i = 0; data.map[i] != NULL; i++)
+		free(data.map[i]);
+	free(data.map);
 
-	// Cleanup
-	mlx_delete_image(mlx, img);
-	mlx_terminate(mlx);
-
-	return EXIT_SUCCESS;
+	return (0);
 }
+
--- so_long/so_long.h
+++ so_long/so_long.h
@@ -1,12 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   so_long.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rlane <rlane@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/06 19:28:50 by rlane             #+#    #+#             */
+/*   Updated: 2024/06/06 19:28:51 by rlane            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef SO_LONG_H
-#define SO_LONG_H
+# define SO_LONG_H
 
-#include "MLX42.h"
-#include <stdint.h>
-#include <stdlib.h>
-#include <stdio.h>
+# include <stdint.h>
+# include <stdlib.h>
+# include <stdio.h>
 
-// Function prototype for drawing a line
-void draw_line(mlx_image_t *img, int x0, int y0, int x1, int y1, uint32_t color);
+
+
 
 #endif // SO_LONG_H
