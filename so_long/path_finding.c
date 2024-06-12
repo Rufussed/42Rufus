8bdacf713727b8f2a007611e9c507c0d25d31a28
Rufussed <rufusjlane@gmail.com>
2024-06-12 15:55:26 +0200

    working better but still double free error

--- so_long/path_finding.c
+++ so_long/path_finding.c
@@ -6,7 +6,7 @@
 /*   By: rlane <rlane@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/11 11:57:29 by rlane             #+#    #+#             */
-/*   Updated: 2024/06/12 15:28:15 by rlane            ###   ########.fr       */
+/*   Updated: 2024/06/12 15:52:06 by rlane            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -54,15 +54,14 @@ void	free_path_data(t_path_data *path_data)
 int	check_all_player_accessible_nodes(t_data *data)
 {
 	t_path_data	*path_data;
-	t_node		*node_visited;
 	int			result;
 
 	path_data = malloc(sizeof(t_path_data));
 	if (!path_data)
 		return (0);
 	path_data->current_node = malloc(sizeof(t_node));
-	node_visited = malloc(sizeof(t_node));
-	if (!path_data->current_node || !node_visited)
+	path_data->cnv = malloc(sizeof(t_node));
+	if (!path_data->current_node || !path_data->cnv)
 	{
 		free(path_data);
 		return (0);
@@ -71,26 +70,28 @@ int	check_all_player_accessible_nodes(t_data *data)
 	ft_printf("created path_data & current_node\n\n");//debug
 	path_data->current_node->x = data->player_x;
 	path_data->current_node->y = data->player_y;
-	node_visited->x = data->player_x;
-	node_visited->y = data->player_y;
+	path_data->cnv->x = data->player_x;
+	path_data->cnv->y = data->player_y;
 	ft_printf("Player x = %d\n", data->player_x);//debug
 	ft_printf("Player y = %d\n", data->player_y);//debug
-	path_data->visited_nodes = ft_lstnew(node_visited);
+	path_data->visited_nodes = ft_lstnew(path_data->cnv);
 	path_data->queued_nodes = ft_lstnew(path_data->current_node);
-	ft_printf("initiallised queue_node\n\n");//debug
+	ft_printf("\ninitiallised queue_node\n\n");//debug
 	while (path_data->queued_nodes)
 	{
 		path_data->current_node = (t_node *)path_data->queued_nodes->content;
 		lst_remove_front(&path_data->queued_nodes);
-		ft_printf("current_node x = %d\n", path_data->current_node->x);//debug
-		ft_printf("current_node y = %d\n\n", path_data->current_node->y);//debug
 		find_adjacent_nodes(data, path_data);
+		path_data->cnv = malloc(sizeof(t_node));
+		path_data->cnv->x = path_data->current_node->x;
+		path_data->cnv->y = path_data->current_node->y;
+		ft_printf("visited: %d, %d\n", path_data->cnv->x, path_data->cnv->y);//debug
 		ft_lstadd_back(&path_data->visited_nodes, 
-			ft_lstnew(path_data->current_node));
+			ft_lstnew(path_data->cnv));
 	}
-	ft_printf("finished finding adjacent nodes\n\n");//debug
+	ft_printf("\nfinished finding adjacent nodes\n\n");//debug
 	result = check_visited_for_keys_exit(data, path_data);
-	free(node_visited);
+	free(path_data->cnv);
 	free_path_data(path_data);
 	return (result);
 }
@@ -170,6 +171,3 @@ void	check_and_queue_node(int x, int y, t_data *data, t_path_data *path_data)
 			free(adjacent_node);
 	}
 }
-
-
-
